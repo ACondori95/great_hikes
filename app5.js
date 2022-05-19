77f8a0dd145bf8900f701e6b9268597b5799815e
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2022-05-19 16:06:33 -0300

    finished

--- app5.js
+++ app5.js
@@ -1,5 +1,5 @@
-import HikesController from "./hikesController";
-// on load grab the array and insert it to the page
+import HikesController from './hikesController.js';
+//on load grab the array and insert it into the page
 const myHikesController = new HikesController('hikes');
 window.addEventListener('load', () => {
   myHikesController.showHikeList();
--- hikesController.js
+++ hikesController.js
@@ -1,6 +1,6 @@
-// The controller nedds access to both the model and the view... so let's import them
-import HikeModel from "./hikeModel";
-import HikesView from "./hikesView";
+// The controller needs access to both the model and the view...so let's import them
+import HikeModel from './hikeModel.js';
+import HikesView from './hikesView.js';
 
 // Just like with the view we should organize the functions we need to our controller. Let's use a class for this one
 
@@ -14,9 +14,9 @@ export default class HikesController {
   showHikeList() {
     // the list of hikes will come from the model now...
     const hikeList = this.hikeModel.getAllHikes();
-    // send the list of hikes and the element we would like those placed into to the view
+    // send the list of hikes and the element we would like those placed into to the view.
     this.hikesView.renderHikeList(this.parentElement, hikeList);
-    // after the hikes have been rendered... add our listener
+    // after the hikes have been rendered...add our listener
     this.addHikeListener();
   }
   showOneHike(hikeName) {
@@ -24,13 +24,13 @@ export default class HikesController {
     this.hikesView.renderOneHikeFull(
       this.parentElement,
       hike
-    ).ontouchend = () => {
+    ).onclick = () => {
       this.showHikeList();
     };
   }
-  // in order to show the details of a hike ontouched we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
+  // in order to show the details of a hike ontouchend we will need to attach a listener AFTER the list of hikes has been built. The function below does that.
   addHikeListener() {
-    // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList... not an array. So in order to use something like a forEach we need to convert it to an array.
+    // We need to loop through the children of our list and attach a listener to each, remember though that children is a nodeList...not an array. So in order to use something like a forEach we need to convert it to an array.
     const childrenArray = Array.from(this.parentElement.children);
     childrenArray.forEach(child => {
       child.addEventListener('click', e => {
